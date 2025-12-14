a974e929bfec16fe1984c5c5f8d8dfeef1fb7eca
yuttto0024 <yuto0024@outlook.com>
2025-12-14 19:52:08 +0900

    feat/table_ft_print_hex.c

--- ft_print_hex.c
+++ ft_print_hex.c
@@ -6,7 +6,27 @@
 /*   By: yuonishi <yuonishi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 17:33:10 by yuonishi          #+#    #+#             */
-/*   Updated: 2025/12/14 17:33:11 by yuonishi         ###   ########.fr       */
+/*   Updated: 2025/12/14 19:51:45 by yuonishi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+static int	ft_puthex_recursive(unsigned int n, char *base)
+{
+	int	len;
+
+	len = 0;
+	while (n >= 16)
+		len += ft_puthex_recursive(n / 16);
+	len += write(1, &base[n % 16], 1);
+	return (len);
+}
+
+int	ft_puthex_low(va_list ap)
+{
+	return (ft_puthex_recursive(va_arg(ap, unsigned int), "0123456789abcdef"))
+}
+
+int	ft_puthex_up(va_list ap)
+{
+	return (ft_puthex_recursive(va_arg(ap, unsigned int), "0123456789ABCDEF"));
+}
---
+++ ft_print_num.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnum.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuonishi <yuonishi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/14 17:33:16 by yuonishi          #+#    #+#             */
+/*   Updated: 2025/12/14 19:41:27 by yuonishi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+static int	ft_putnbr_recursive(long n)
+{
+	int		len;
+	char	c;
+	
+	len = 0;
+	if (n < 0)
+	{
+		len += write(1, "-", 1);
+		n = -n;
+	}
+	if (n >= 10)
+		len += ft_putnbr_recursive(n / 10);
+	c = (n % 10) + '0';
+	len += write(1, &c, 1);
+	return (len);
+}
+
+int	ft_putnbr(va_list ap)
+{
+	int	n;
+
+	n = va_arg(ap, int);
+	return (ft_putnbr_recursive((long)n));
+}
+
+static int	ft_putnbr_unsigned_recursive(unsigned int n)
+{
+	int		len;
+	char	c;
+
+	len = 0;
+	if (n >= 10)
+		len += ft_punbr_unsigned_recursive(n / 10);
+	c = (n % 10) + '0';
+	len += write(1, &c, 1)
+	return (len);
+}
+
+int	ft_putnbr_unsigned(va_list ap)
+{
+	unsigned int	n;
+
+	n = va_arg(ap, unsigned int);
+	return (ft_punbr_unsigned_recursive(n));
+}
